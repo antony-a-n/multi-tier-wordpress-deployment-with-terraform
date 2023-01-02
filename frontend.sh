85ce9ff9fd16fe3d2508193ae6f01fd76497a888
Antony A N <61390678+antony-a-n@users.noreply.github.com>
2023-01-02 23:51:52 +0530

    Create  frontend.sh

---
+++ frontend.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+domain=db.antonyan.local
+db_root_password=qwertyuiop@123
+db_name=wp_db
+db_user=dbuser
+db_password=wp123
+sudo yum update all -y
+sudo yum install httpd -y
+amazon-linux-extras install php7.4 
+sudo systemctl restart httpd
+sudo systemctl enable httpd
+cd /var/www/html
+sudo chown -R apache:apache /var/www/html/*
+wget -c http://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+cp -rf wordpress/* /var/www/html/
+mv wp-config-sample.php wp-config.php
+sed -i "s/database_name_here/$db_name/;s/username_here/$db_user/;s/password_here/$db_password/;s/localhost/$domain/" wp-config.php
+sudo systemctl stop httpd
+sudo systemctl restart httpd
