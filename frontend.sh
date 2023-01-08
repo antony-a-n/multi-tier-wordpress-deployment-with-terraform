b59c7a7db4e7f9106b1a8512c0ec2494baf2f89e
Antony A N <61390678+antony-a-n@users.noreply.github.com>
2023-01-08 14:39:42 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-domain=db.antonyan.local
-db_root_password=qwertyuiop@123
-db_name=wp_db
-db_user=dbuser
-db_password=wp123
 sudo yum update all -y
 sudo yum install httpd -y
-amazon-linux-extras install php7.4 
+amazon-linux-extras install php7.4
 sudo systemctl restart httpd
 sudo systemctl enable httpd
 cd /var/www/html
@@ -15,6 +10,6 @@ wget -c http://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 cp -rf wordpress/* /var/www/html/
 mv wp-config-sample.php wp-config.php
-sed -i "s/database_name_here/$db_name/;s/username_here/$db_user/;s/password_here/$db_password/;s/localhost/$domain/" wp-config.php
+sed -i "s/database_name_here/${db_name}/;s/username_here/${db_user}/;s/password_here/${db_password}/;s/localhost/${domain}/" wp-config.php
 sudo systemctl stop httpd
-sudo systemctl restart httpd
+sudo systemctl restart httpd   
