9985d5c77286cf1e43f24003b7c5487aff1ddd46
Antony A N <61390678+antony-a-n@users.noreply.github.com>
2023-01-08 12:39:45 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,20 @@
-output "URL"{
+output "url" {
 
-    value = "http://${aws_route53_record.www-url.name}"
-} 
+  value = "http://${aws_route53_record.www-url.name}"
+}
 
-output "public-dns-name"{
-    value = aws_instance.front-end-server.public_dns 
+output "dns" {
+  value = aws_instance.front-end-server.public_dns
+}
+
+output "public_ip" {
+  value = aws_instance.front-end-server.public_ip
+}
+
+
+output "db-host" {
+  value = local.db-host
+}
+output "vpc-module-return" {
+  value = module.vpc
 }
