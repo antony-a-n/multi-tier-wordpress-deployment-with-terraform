c4547bcb1adac59e358f8baa084c8e6a9442b3c6
Antony A N <61390678+antony-a-n@users.noreply.github.com>
2023-01-08 12:51:32 +0530

    Update datasource.tf

--- datasource.tf
+++ datasource.tf
@@ -6,3 +6,24 @@ data "aws_route53_zone" "mydomain" {
   name         = var.domain
   private_zone = false
 }
+
+data "template_file" "db" {
+  template = "${file("${path.module}/mysql.sh")}"
+  vars = {
+    db_name = var.database
+    db_user = var.database-user
+    db_password = var.database-password
+    db_root_password = var.root-password
+  }
+}
+
+data "template_file" "web"{
+  template = "${file("${path.module}/frontend.sh")}"
+  vars = {
+    domain = local.db-host
+    db_name = var.database
+    db_user = var.database-user
+    db_password = var.database-password
+    db_root_password = var.root-password
+  }
+}
